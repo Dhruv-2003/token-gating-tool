7490040a3ae3ccff61821710f40c86c4c069763a
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-03-06 13:21:40 +0530

    chore: Examples With Universal and React Wrappers added

--- example/react-demo-app/src/index.tsx
+++ example/react-demo-app/src/index.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { UniversalGatingWrapper } from "token-gating-sdk";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { configData } from "./config/config";
+const API_KEY: any = process.env.REACT_APP_ALCHEMY_API_KEY;
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <UniversalGatingWrapper config={configData} alchemyApiKey={API_KEY}>
+      <App />
+    </UniversalGatingWrapper>
   </React.StrictMode>
 );
 
---
+++ example/react-demo-app/src/privatetokenamount.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const privatetokenAmount = () => {
+  return <div>privatetokenAmount</div>;
+};
+
+export default privatetokenAmount;
